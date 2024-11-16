6052edea802806228140ea4114f8d81833073091
abdul rahman <abdul19rehmankhan@gamil.com>
2024-11-16 19:22:50 +0530

    Social-media-tw

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    
+    extend: {
+      colors: {
+        white:'#FFFFFF',
+        grey:'#333333',
+        black:'#141414',
+        darkgrey:'#1F1F1F',
+        neongreen:'#C4F82A'
+
+      }
+    },
+  },
+  plugins: [],
+}
